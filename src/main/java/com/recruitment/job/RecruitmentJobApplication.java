93e4c94264726f56b6b0a18e7f513037d7de135e
Marlo <wnuka1337@gmail.com>
2023-07-24 16:14:09 +0200

    Initialize project

---
+++ src/main/java/com/recruitment/job/RecruitmentJobApplication.java
@@ -0,0 +1,13 @@
+package com.recruitment.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recruitment/job/RecruitmentJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recruitment.job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
